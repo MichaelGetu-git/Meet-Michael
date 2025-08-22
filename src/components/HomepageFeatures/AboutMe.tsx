6dd9e52d4242aa872ebf0bf279e55338a0a60fd4
MichaelGetu-git <michaelgetu21@gmail.com>
2025-08-22 23:42:34 +0300

    update

--- src/components/HomepageFeatures/AboutMe.tsx
+++ src/components/HomepageFeatures/AboutMe.tsx
@@ -2,7 +2,7 @@ import { JSX } from "react";
 
 export default function AboutMe(): JSX.Element {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center">
             <h1>About Me</h1>
         </div>
     )
